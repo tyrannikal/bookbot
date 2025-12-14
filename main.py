1567f5926dacb11166583279efe805fd31ff54db
tyrannikal <101421596+tyrannikal@users.noreply.github.com>
2025-12-14 14:58:32 -0500

    add get_num_chars function

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from stats import get_book_text, get_num_words
+from stats import get_book_text, get_num_words, get_num_chars
 
 def main():
-    
-    word_count = get_num_words("./books/frankenstein.txt")
+   
+    filepath = "./books/frankenstein.txt"
+
+    word_count = get_num_words(filepath)
     print(f"Found {word_count} total words")
+
+    char_count = get_num_chars(get_book_text(filepath))
+    print(char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,21 @@ def get_num_words(filepath):
     words = get_book_text(filepath).split()
 
     return len(words)
+
+def get_num_chars(book_text):
+
+    words = book_text.split()
+    num_chars = {}
+
+    for word in words:
+
+        lower_word = word.lower()
+        for char in lower_word:
+
+            if char in num_chars:
+
+                num_chars[char] += 1
+            else:
+                num_chars[char] = 1
+
+    return num_chars
