3ab3950dfdbd5030ec8c37efa2709842262e1138
tyrannikal <101421596+tyrannikal@users.noreply.github.com>
2025-12-14 21:02:12 -0500

    replaced hard-coded filepath with system argument usage

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import sys
 from stats import get_book_text, get_num_words, get_num_chars, create_report
 
 def main():
-   
-    filepath = "books/frankenstein.txt"
+ 
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
     word_count = get_num_words(filepath)
     char_count = get_num_chars(get_book_text(filepath))
     sorted_report = create_report(char_count)
@@ -14,7 +19,6 @@ def main():
     print("--------- Character Count -------")
 
     for dict_tmp in sorted_report:
-
         print(f"{dict_tmp["char"]}: {dict_tmp["num"]}")
 
     print("============= END ===============")
