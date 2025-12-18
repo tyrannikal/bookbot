c07040acc922bc98c28a46991750832ee76a6864
tyrannikal <101421596+tyrannikal@users.noreply.github.com>
2025-12-18 18:25:21 -0500

    added black formatting

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
 from stats import get_book_text, get_num_words, get_num_chars, create_report
 
+
 def main():
- 
+
     if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
@@ -22,5 +23,6 @@ def main():
         print(f"{dict_tmp["char"]}: {dict_tmp["num"]}")
 
     print("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,16 +1,17 @@
 def get_book_text(filepath):
 
     with open(filepath) as f:
-
         file_contents = f.read()
 
     return file_contents
+
 
 def get_num_words(filepath):
 
     words = get_book_text(filepath).split()
 
     return len(words)
+
 
 def get_num_chars(book_text):
 
@@ -23,25 +24,25 @@ def get_num_chars(book_text):
         for char in lower_word:
 
             if char in num_chars:
-
                 num_chars[char] += 1
             else:
                 num_chars[char] = 1
 
     return num_chars
 
+
 def sort_on(items):
     return items["num"]
+
 
 def create_report(num_chars):
 
     dict_tmp = {}
-    result = [] 
+    result = []
 
     for char in num_chars:
-        
-        if char.isalpha():
 
+        if char.isalpha():
             dict_tmp["char"] = char
             dict_tmp["num"] = num_chars[char]
             result.append(dict_tmp)
